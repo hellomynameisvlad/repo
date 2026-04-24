8f16c46f7d9a9e00e68d58c802c0bfef4f51a1f6
hellomynameisvlad <guzenko.vlad@gmail.com>
2026-04-24 18:08:18 +0200

    Simplify locale detection and update widget settings
    Refactor locale detection and widget settings initialization.

--- script.js
+++ script.js
@@ -1,52 +1,13 @@
-
-function detectLocalesFromPath() {
-  const path = window.location.pathname.toLowerCase();
-
-  if (path.includes("/sv/")) {
-    return { pageLocale: "sv-SE", widgetLocale: "sv" };
-  }
-
-  if (path.includes("/no/")) {
-    return { pageLocale: "nb-NO", widgetLocale: "no" };
-  }
-
-  if (path.includes("/en/")) {
-    return { pageLocale: "en-US", widgetLocale: "en-US" };
-  }
-
-  return { pageLocale: "en-US", widgetLocale: "en-US" };
-}
-
-const { pageLocale, widgetLocale } = detectLocalesFromPath();
-
-window.zESettings = {
-  webWidget: {
-    locale: widgetLocale
-  }
-};
-
 (function () {
-  const s = document.createElement("script");
-  s.id = "ze-snippet";
-  s.src = "https://static.zdassets.com/ekr/snippet.js?key=a9cae50f-cf32-493d-9478-656245c0e674";
-  s.async = true;
+  const interval = setInterval(() => {
+    if (!window.zE) return;
 
-  s.onload = function () {
-    const timer = setInterval(() => {
-      if (!window.zE) return;
+    clearInterval(interval);
 
-      clearInterval(timer);
+    window.zE('messenger:set', 'conversationFields', [
+      { id: '46010718065809', value: window.location.href }
+    ]);
 
-      zE("messenger:set", "conversationFields", [
-        { id: "26983370877724", value: pageLocale }
-      ]);
-
-      zE("messenger:set", "locale", widgetLocale);
-
-      console.log("pageLocale:", pageLocale);
-      console.log("widgetLocale:", widgetLocale);
-    }, 200);
-  };
-
-  document.head.appendChild(s);
+    console.log('Zendesk field set:', window.location.href);
+  }, 200);
 })();
